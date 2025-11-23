14bb54b0d6215c7de0c7ab3d6a6f4da0a9d4d445
Mikhail Solovyov <solovev02@yandex.ru>
2025-11-23 19:21:45 +0300

    minor update

--- src/test/java/StatusTests.java
+++ src/test/java/StatusTests.java
@@ -1,2 +1,48 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+
+
 public class StatusTests {
+    @Test
+    void checkTotal5(){
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(5));
+    }
+
+    @Test
+    void checkTotalWithResponseLogs() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total", is(5));
+    }
+
+    @Test
+    void checkTotalWithLogs() {
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().body()
+                .body("total", is(5));
+    }
+
+    @Test
+    void checkTotalWithStatusLogs() {
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(5))
+                .body("browsers.chrome", hasKey("127.0"));
+    }
+
 }
